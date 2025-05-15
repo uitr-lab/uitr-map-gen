29e85d0d4c55fff62b1ed2adbff5bc90d3619ce8
nickolanack <nickblackwell82@gmail.com>
2025-05-14 21:31:42 -0700

    initial commit - no libs

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='uitr-map-gen',
+    version='0.1.0',
+    description='quick hex/grid/square/polygon map generators for interactive histogram maps',
+    author='Nick Blackwell',
+    author_email='nick.blackwell@ubc.ca',
+    packages=find_packages(),  # Automatically finds all packages
+    install_requires=[
+        "geopandas>=1.0.1", "pandas>=2.2.3", "openpyxl>=3.1.5",
+    ],
+    python_requires='>=3.11',
+)
